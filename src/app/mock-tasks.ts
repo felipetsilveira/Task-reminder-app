83f2500404609180b7c934ba87c8e429869f2ba0
AUTH\SantosFe <felipe.santos@hp.com>
2022-07-06 02:03:19 -0300

    Add components and services

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,40 @@
+import {Task} from './Tasks'
+
+export const TASKS: Task[] = [
+{
+  id: 1,
+  text: 'Doctors appointment',
+  day: 'May 5th at 2pm',
+  reminder: true
+},
+{
+  id: 2,
+  text: 'Encontro com os amigos',
+  day: '3 de janeiro',
+  reminder: true
+},
+{
+  id: 3,
+  text: 'Jantar de formatura da fulana',
+  day: '01 de setembro',
+  reminder: true
+},
+{
+  id: 4,
+  text: 'Abertura dos jogos olimpicos',
+  day: '04 de setembro',
+  reminder: true
+},
+{
+  id: 5,
+  text: 'Jantar legal',
+  day: '21 de setembro',
+  reminder: true
+},
+{
+  id: 6,
+  text: 'Café da fulana',
+  day: '17 de setembro',
+  reminder: true
+},
+]
